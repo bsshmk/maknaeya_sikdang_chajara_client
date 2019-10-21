7d654d3c052462b3e072b46de7235cfdc333ed62
ChoMk <39166150+ChoMk@users.noreply.github.com>
2019-10-21 14:37:53 +0900

    http에서 송수신이 가능하도록 수정
    http에서 송수신이 가능하도록 수정

--- maknaeya_sikdang_chajara/app/src/main/java/com/mksoft/maknaeya_sikdang_chajara/utils/Constants.kt
+++ maknaeya_sikdang_chajara/app/src/main/java/com/mksoft/maknaeya_sikdang_chajara/utils/Constants.kt
@@ -2,5 +2,5 @@ package com.mksoft.maknaeya_sikdang_chajara.utils
 
 //고정으로 미리 정하는 변수들...
 
-const val BASE_URL: String = "http://165.246.182.112:9999"
+const val BASE_URL: String = "http://165.246.241.117:9999"
 const val NCP_client_ID:String = "1metpf57d9"
